d8d1198c139d7ccc54b41b6e502dde8407d6e78f
Yfill <vps998@gmail.com>
2020-10-24 02:26:25 +0800

    perf: streamlined style creation process

--- src/style/index.ts
+++ src/style/index.ts
@@ -3,17 +3,7 @@ import Border from './border';
 import Font from './font';
 import Shadow from './shadow';
 import { calcBackgroundColor, calcBorderColor, calcFontColor } from '../utils/calc-color';
-import { createStyleItem } from '../utils/craete-style-item';
-import { curry } from '../utils/curry';
 
-const createId = (
-  mark:StyleMark,
-  name:string,
-) => (
-  mark
-    ? `theme-${name}-${mark}`
-    : `theme-${name}`
-);
 export const createStyle = (
   {
     color,
@@ -25,53 +15,47 @@ export const createStyle = (
     fontColorGroup,
     mark,
   }: StyleOptions,
-  commonThemeOpt:CommonThemeOpt,
+  commonThemeOpt: CommonThemeOpt,
 ): Style => {
   const commonOpts = {
     maxLevel: commonThemeOpt.maxLevel,
     prefix: commonThemeOpt.prefix,
     mark,
   };
-  const createMarkId = curry(createId, mark);
   const styleItems = [
-    createStyleItem(
-      new Background({
-        ...commonOpts,
-        color: backgroundColor || calcBackgroundColor(color, mark),
-        colorGroup: backgroundColorGroup,
-      }),
-      createMarkId('background'),
-    ),
-    createStyleItem(
-      new Border({
-        ...commonOpts,
-        color: borderColor || calcBorderColor(color, mark),
-        colorGroup: borderColorGroup,
-      }),
-      createMarkId('border'),
-    ),
-    createStyleItem(
-      new Font({
-        ...commonOpts,
-        color: fontColor || calcFontColor(color, mark),
-        colorGroup: fontColorGroup,
-      }, commonThemeOpt.minFontSize, commonThemeOpt.maxFontSize),
-      createMarkId('font'),
-    ),
-    createStyleItem(
-      new Shadow({
-        ...commonOpts,
-        color,
-      }),
-      createMarkId('shadow'),
-    ),
+    new Background({
+      ...commonOpts,
+      color: backgroundColor || calcBackgroundColor(color, mark),
+      colorGroup: backgroundColorGroup,
+    }),
+    new Border({
+      ...commonOpts,
+      color: borderColor || calcBorderColor(color, mark),
+      colorGroup: borderColorGroup,
+    }),
+    new Font({
+      ...commonOpts,
+      color: fontColor || calcFontColor(color, mark),
+      colorGroup: fontColorGroup,
+    }, commonThemeOpt.minFontSize, commonThemeOpt.maxFontSize),
+    new Shadow({
+      ...commonOpts,
+      color,
+    }),
   ];
   return {
     mount() {
-      styleItems.forEach((styleItem) => styleItem.init());
+      const themeStyleWrap = document.head;
+      if (themeStyleWrap.querySelector(`[theme-item="${mark}"]`)) return;
+      const target = document.createElement('style');
+      target.innerText = styleItems.map((styleItem) => styleItem.exportStyle()).join('');
+      target.setAttribute('theme-item', mark);
+      themeStyleWrap.appendChild(target);
     },
     umount() {
-      styleItems.forEach((styleItem) => styleItem.destroy());
+      const themeStyleWrap = document.head;
+      const el = themeStyleWrap.querySelector(`[theme-item="${mark}"]`);
+      if (el) themeStyleWrap.removeChild(el);
     },
   };
 };
