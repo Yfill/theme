44dac8cf148738fdbf0821e9709220af3f4d5956
Yfill <vps998@gmail.com>
2020-10-27 02:21:12 +0800

    feat: add a method to remove empty items from the array

--- src/utils/array.ts
+++ src/utils/array.ts
@@ -3,6 +3,10 @@ export const arrayIncludes = (items: any[], item: any) => {
   for (let index = 0; index < length; index += 1) if (items[index] === item) return true;
   return false;
 };
+export const arrayFilterEmptyItem = (
+  items: any[],
+) => items.filter((item) => item !== null && item !== undefined);
 export default {
   arrayIncludes,
+  arrayFilterEmptyItem,
 };
