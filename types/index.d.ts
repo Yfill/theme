4aae00351fcbf5ee707f201ed9747b10754df773
Yfill <vps998@gmail.com>
2020-10-31 21:55:29 +0800

    build: build 1.0.6

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,119 +1,97 @@
-export declare type ThemeMode = 'light' | 'dark'
-export declare type ThemeStatus = 'mounted' | 'unmounted' | 'notMounted'
-export declare type StyleMark = string
+import EventHub from '@yfill/event-hub';
+declare type ThemeMode = 'light' | 'dark';
+declare type ThemeStatus = 'mounted' | 'unmounted' | 'notMounted';
 export declare type Style = {
-    mount: Function,
-    umount: Function
-}
-export declare type ColorGroup = string[]
-export declare type NameMap = { [index: string]: string }
-export declare type StyleOptions = {
-    mark: StyleMark,
-    color: string,
-    backgroundColor?: string,
-    borderColor?: string,
-    fontColor?: string,
-    backgroundColorGroup?: ColorGroup
-    backgroundColorNameMap?: NameMap
-    borderColorGroup?: ColorGroup
-    borderColorNameMap?: NameMap
-    fontColorGroup?: ColorGroup
-    fontColorNameMap?: NameMap,
-    fontSizeNameMap?: NameMap,
-    boxShadowNameMap?: NameMap,
-}
-export declare type ThemeOpt = {
-    lightOpt?: StyleOptions,
-    darkOpt?: StyleOptions,
-    mainOpt?: StyleOptions,
-    mode?: ThemeMode,
-    prefix?: string,
-    minFontSize?: number,
-    maxFontSize?: number,
-    maxLevel?: number,
-}
+    mount: Function;
+    umount: Function;
+};
 export declare type CommonThemeOpt = {
-    prefix: string,
-    minFontSize?: number,
-    maxFontSize?: number,
-    maxLevel?: number
+    prefix: string;
+    minFontSize?: number;
+    maxFontSize?: number;
+    maxLevel?: number;
+};
+export declare type ColorGroup = string[];
+export declare type StyleMark = string;
+export declare type NameMap = {
+    [index: string]: string;
+};
+export declare type StyleOptions = {
+    mark: StyleMark;
+    color: string;
+    backgroundColor?: string;
+    borderColor?: string;
+    fontColor?: string;
+    backgroundColorGroup?: ColorGroup;
+    backgroundColorNameMap?: NameMap;
+    borderColorGroup?: ColorGroup;
+    borderColorNameMap?: NameMap;
+    fontColorGroup?: ColorGroup;
+    fontColorNameMap?: NameMap;
+    fontSizeNameMap?: NameMap;
+    boxShadowNameMap?: NameMap;
+};
+declare type Store = {
+    lightStyleInstance: Style | null;
+    darkStyleInstance: Style | null;
+    mainStyleInstance: Style | null;
+    otherStyleInstanceMap: {
+        [prop: string]: Style;
+    };
+    commonThemeOpt: CommonThemeOpt;
+    initialLightOpt: StyleOptions | null;
+    initialDarkOpt: StyleOptions | null;
+    initialMainOpt: StyleOptions | null;
+};
+declare type ThemeOpt = {
+    lightOpt?: StyleOptions;
+    darkOpt?: StyleOptions;
+    mainOpt?: StyleOptions;
+    mode?: ThemeMode;
+    prefix?: string;
+    minFontSize?: number;
+    maxFontSize?: number;
+    maxLevel?: number;
+};
+declare type ThemePlugin = {
+    install: Function;
+    uninstall: Function;
+};
+declare type Handler = (...arg: any[]) => void;
+export default class Theme {
+    static EventHub: typeof EventHub;
+    static themeInstance: Theme | null;
+    static run(themeOpt?: ThemeOpt): Theme;
+    static mount(): Theme | undefined;
+    static umount(): Theme | undefined;
+    static add(styleOpt: StyleOptions): Theme | undefined;
+    static remove(styleMark: StyleMark): Theme | undefined;
+    static update(styleOpt: StyleOptions): Theme | undefined;
+    static refresh(): Theme | undefined;
+    static change(mode?: ThemeMode): Theme | undefined;
+    static install(plugin: ThemePlugin, ...arg: any[]): Theme | undefined;
+    static use(plugin: ThemePlugin, ...arg: any[]): Theme | undefined;
+    static uninstall(plugin: ThemePlugin): Theme | undefined;
+    static getStore(): Store | undefined;
+    static on(type: string, handler: Handler): Theme | undefined;
+    static off(type: string, handler: Handler): Theme | undefined;
+    static emit(type: string, ...arg: any[]): Theme | undefined;
+    mode: ThemeMode;
+    status: ThemeStatus;
+    constructor(themeOpt?: ThemeOpt);
+    mount(): Theme;
+    umount(): Theme;
+    add(styleOpt: StyleOptions): Theme;
+    remove(styleMark: StyleMark): Theme;
+    update(styleOpt: StyleOptions): Theme;
+    refresh(): Theme;
+    change(mode?: ThemeMode): Theme;
+    getStore(): Store;
+    install(plugin: ThemePlugin, ...arg: any[]): Theme;
+    use(plugin: ThemePlugin, ...arg: any[]): Theme;
+    uninstall(plugin: ThemePlugin): Theme;
+    on(type: string, handler: Handler): Theme;
+    off(type: string, handler: Handler): Theme;
+    emit(type: string, ...arg: any[]): Theme;
 }
-export declare type Store = {
-    lightStyleInstance: Style | null,
-    darkStyleInstance: Style | null,
-    mainStyleInstance: Style | null,
-    otherStyleInstanceMap: { [prop: string]: Style },
-    commonThemeOpt: CommonThemeOpt = { prefix: '' },
-    initialLightOpt: StyleOptions | null,
-    initialDarkOpt: StyleOptions | null,
-    initialMainOpt: StyleOptions | null,
-}
-export declare type Handler = ((...arg: any[]) => void) & { [id: string]: number };
-export declare class Theme {
-    constructor(themeOpt?: ThemeOpt): void
-
-    static themeInstance: Theme | null
-
-    static run(themeOpt?: ThemeOpt): Theme
-
-    static mount(): Theme | undefined
-
-    static umount(): Theme | undefined
-
-    static add(styleOpt: StyleOptions): Theme | undefined
-
-    static remove(styleMark: StyleMark): Theme | undefined
-
-    static update(styleOpt: StyleOptions): Theme | undefined
-
-    static refresh(): Theme | undefined
-
-    static change(mode?: ThemeMode): Theme | undefined
-
-    static install(plugin: ThemePlugin, ...arg: any[]): Theme | undefined
-
-    static use(plugin: ThemePlugin, ...arg: any[]): Theme | undefined
-
-    static uninstall(plugin: ThemePlugin): Theme | undefined
-
-    static getStore(): Store | undefined
-
-    static on(type: string, handler: Handler): Theme | undefined
-
-    static off(type: string, handler: Handler): Theme | undefined
-
-    static emit(type: string, ...arg: any[]): Theme | undefined
-
-    mode: ThemeMode
-
-    status: ThemeStatus
-
-    mount(): Theme
-
-    umount(): Theme
-
-    add(styleOpt: StyleOptions): Theme
-
-    remove(styleMark: StyleMark): Theme
-
-    update(styleOpt: StyleOptions): Theme
-
-    refresh(): Theme
-
-    change(mode?: ThemeMode): Theme
-
-    getStore(): Store
-
-    install(plugin: ThemePlugin, ...arg: any[]): Theme
-
-    use(plugin: ThemePlugin, ...arg: any[]): Theme
-
-    uninstall(plugin: ThemePlugin): Theme
-
-    on(type: string, handler: Handler): Theme
-
-    off(type: string, handler: Handler): Theme
-
-    emit(type: string, ...arg: any[]): Theme
-}
-export default Theme;
+export {};
